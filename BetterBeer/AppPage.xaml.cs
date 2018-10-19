f424e624f1089fdddfedc9d5f4db7fd07ea49eba
Cleit92 <hass.marc@gmx.net>
2018-10-19 22:59:27 +0200

    First-Build

---
+++ BetterBeer/AppPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BetterBeer
+{
+    public partial class AppPage : ContentPage
+    {
+        public AppPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btn_login_clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NavigationPage(new MenuPage()));
+        }
+    }
+}
---
+++ BetterBeer/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BetterBeer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_clicked(object sender, EventArgs e)
+        {
+            string email = entry_email.Text;
+            string password = entry_password.Text;
+
+            App.Current.MainPage = new NavigationPage(new AppPage());
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new AppPage());
+        }
+    }
+}
