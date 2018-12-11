233c92fd24f3537fba6261b8463d3571f51384a3
Johannes Peretzke <johannes.peretzke@googlemail.com>
2018-12-11 13:22:37 +0100

    Bugfix: BierBewertung nur Kriterien der id 1-5 bewertet und nicht je nach Kritierium richtig

--- BetterBeer/Objects/Database.cs
+++ BetterBeer/Objects/Database.cs
@@ -457,20 +457,21 @@ namespace BetterBeer
         }
 
 
-        public static bool CreateRating(int beerID, int userID, List<int> rating)
+        public static bool CreateRating(int beerID, int userID, List<int> rating,List<Criteria> criterias)
         {
             string postData = $"beerId={beerID}&userId={userID}";
             int ratingID = int.Parse(apiCall("createRating", postData));
 
+           
             if (ratingID > 0)
             {
-                for (int i = 1; i <= rating.Count; i++)
+                int i = 0;
+                foreach (Criteria crit in criterias)
                 {
-                    postData = $"ratingId={ratingID}&critId={i}&grade={rating[i - 1]}";
+                    postData = $"ratingId={ratingID}&critId={crit.KriterienID}&grade={rating[i]}";
                     apiCall("createGrade", postData);
-
+                    i++;
                 }
-
                 return true;
             }
 
